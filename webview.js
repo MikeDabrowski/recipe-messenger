a39e088598587d3eb3d809c557519044bcb99be9
MikeDabrowski <mike2060@gmail.com>
2018-09-30 18:31:58 +0200

    Init - dark

--- webview.js
+++ webview.js
@@ -1,4 +1,8 @@
-module.exports = (Franz) => {
+'use strict';
+
+const path = require('path');
+
+module.exports = Franz => {
   const getMessages = function getMessages() {
     let count = document.querySelectorAll('._5fx8:not(._569x),._1ht3:not(._569x)').length;
     const messageRequestsElement = document.querySelector('._5nxf');
@@ -10,7 +14,8 @@ module.exports = (Franz) => {
   };
 
   Franz.loop(getMessages);
-  
+  Franz.injectCSS(path.join(__dirname, 'transparent.css'));
+
   /* Enable desktop notifications in messenger settings */
   localStorage["_cs_desktopNotifsEnabled"] = JSON.stringify({"__t":new Date().getTime(), "__v":true})
 
